a9584da1c7b78684aede35d108d689b5066a3ad6
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-27 14:09:17 +0700

    Implement Test

---
+++ SuperCalculator.Test/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace SuperCalculator.Test
+{
+  [TestFixture]
+  class CalculatorTest
+  {
+    [Test]
+    public void SumTwoInt()
+    {
+      // Arrange
+      var cal = new Calculator();
+      int a = 10;
+      int b = 20;
+
+      // Act 
+      var result = cal.Sum(a, b);
+
+      // Assert
+      int expectedResult = 30;
+      Assert.AreEqual(expectedResult, result);
+    }
+    [Test]
+    public void TestCalculateVAT()
+    {
+      // Arrange
+      var cal = new Calculator();
+      var originalPrice = 123.456;
+      // Act
+      var actualResult = cal.CalculateTotalPriceWithVAT(originalPrice);
+
+      // Assert
+      var expectedResult = 135.8016;
+      Assert.AreEqual(expectedResult, actualResult);
+    }
+  }
+}
